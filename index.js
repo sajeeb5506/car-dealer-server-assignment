c51286901cdff1fae38857dd263b39b271071563
sajeeb5506 <sajeebsarker5506@gmail.com>
2022-05-08 02:04:51 +0600

    get api solve

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT||5000;
 require('dotenv').config();
@@ -11,9 +12,38 @@ app.use(express.json());
 
 
 
+
+const uri = `mongodb+srv://${process.env.CAR_DEALER}:${process.env.CAR_PASS}@cluster0.4qb7o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+
+    try{
+
+        await client.connect();
+        const carItems = client.db('cardealer').collection('carItems');
+
+        //get api 
+        app.get('/cars', async(req,res)=>{
+            const query = {};
+            const cursor = carItems.find(query);
+            const caritems = await cursor.toArray();
+            res.send(caritems);
+
+        });
+
+        // post api
+
+        
+    }
+    finally{
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 app.get('/',(req,res)=>{
 
-    res.send('hello');
+    res.send('hello.');
 
 })
 
